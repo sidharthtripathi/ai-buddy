f49dc5939ae53157c143832e2c80cba1551194b6
Sidharth Tripathi <siharthtripathi807@gmail.com>
2025-08-03 15:10:00 +0530

    fixed build errors

--- apps/frontend/src/components/ui/kibo-ui/code-block/index.tsx
+++ apps/frontend/src/components/ui/kibo-ui/code-block/index.tsx
@@ -507,7 +507,6 @@ export const CodeBlockCopyButton = ({
 
   if (asChild) {
     return cloneElement(children as ReactElement, {
-      // @ts-expect-error - we know this is a button
       onClick: copyToClipboard,
     });
   }
